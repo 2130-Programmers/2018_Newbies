276d67bbdc35eb1c19ed4bc9113c351607c9395f
Alpha Ten <45672860+Alpha2130Ten@users.noreply.github.com>
2018-12-08 12:49:52 -0800

    added DiffDrive
    Added diff drive using axis
    rotate = 0
    Move = 1

--- GitHubTest/src/main/java/frc/robot/RobotMap.java
+++ GitHubTest/src/main/java/frc/robot/RobotMap.java
@@ -14,10 +14,21 @@ package frc.robot;
  * floating around.
  */
 public class RobotMap {
+
+	
   // For example to map the left and right motors, you could define the
   // following variables to use with your drivetrain subsystem.
   // public static int leftMotor = 1;
   // public static int rightMotor = 2;
+	public static final int DRIVETRAIN_LEFT_FRONT_MOTOR = 1;
+	public static final int DRIVETRAIN_LEFT_REAR_MOTOR = 2;
+	public static final int DRIVETRAIN_RIGHT_FRONT_MOTOR = 3;
+  public static final int DRIVETRAIN_RIGHT_REAR_MOTOR = 4;
+
+  public static final int DRIVER_CONTROLLER = 0;
+  
+	public static final int DRIVER_CONTROLLER_MOVE_AXIS = 1;
+	public static final int DRIVER_CONTROLLER_ROTATE_AXIS = 0;
 
   // If you are using multiple modules, make sure to define both the port
   // number and the module. For example you with a rangefinder:
