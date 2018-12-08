e21b3bbb4386baa4fbc5afcef61ea7454a3f88a4
Alpha Two <45672091+Alpha2130Two@users.noreply.github.com>
2018-12-08 14:02:30 -0800

    Added Robot
    Added Robot with differential drive using a controller.

---
+++ LBrinkman/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,55 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.RobotMap;
+import frc.robot.commands.DriveArcade;
+
+public class DriveTrain extends Subsystem {
+
+  WPI_TalonSRX leftFrontMotor = null;
+  WPI_TalonSRX leftRearMotor = null;
+  WPI_TalonSRX rightFrontMotor = null;
+  WPI_TalonSRX rightRearMotor = null;
+
+  DifferentialDrive differentialDrive = null;
+
+  public DriveTrain() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  leftFrontMotor = new WPI_TalonSRX(RobotMap.DRIVETRAIN_LEFT_FRONT_MOTOR);
+  leftRearMotor = new WPI_TalonSRX(RobotMap.DRIVETRAIN_LEFT_REAR_MOTOR);
+  rightFrontMotor = new WPI_TalonSRX(RobotMap.DRIVETRAIN_RIGHT_FRONT_MOTOR);
+  rightRearMotor = new WPI_TalonSRX(RobotMap.DRIVETRAIN_RIGHT_REAR_MOTOR);
+
+  SpeedControllerGroup leftMotors = new SpeedControllerGroup(leftFrontMotor, leftRearMotor);
+  SpeedControllerGroup rightMotors = new SpeedControllerGroup(rightFrontMotor, rightRearMotor);
+
+  differentialDrive = new DifferentialDrive(leftMotors, rightMotors);
+  }
+
+  // Called just before this Command runs the first time
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+
+    setDefaultCommand(new DriveArcade());
+  }
+  
+
+  public void arcadeDrive(double moveSpeed, double rotateSpeed) {
+    differentialDrive.arcadeDrive(moveSpeed, rotateSpeed);
+  } 
+}
